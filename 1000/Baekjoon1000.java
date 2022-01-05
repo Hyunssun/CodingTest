0b8a64546c35614d384cf0a769aaef612cc8685f
Hyunseon <sunny20829@gmail.com>
2022-01-05 14:15:40 +0900

    1000

---
+++ 1000/Baekjoon1000.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Baekjoon1000 {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        int a, b;
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        System.out.println(a + b);
+    }
+}
