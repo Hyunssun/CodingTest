520f84eaaa1ae3cc328253b6306642482b11563f
Hyunsun Park <sunny20829@gmail.com>
2023-08-08 15:46:43 +0900

    [unrated] Title: 더 크게 합치기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,15 +1,4 @@
 function solution(a, b) {
     var answer = 0;
-    let as = a.toString()
-    let bs = b.toString()
-    if(as + bs > bs + as){
-        answer = as + bs
-    }
-    else if(as + bs < bs + as){
-        answer = bs + as
-    }
-    else{
-        answer = as + bs
-    }
-    return Number(answer);
+    return  Math.max(Number(`${a}${b}`),Number(`${b}${a}`));
 }
