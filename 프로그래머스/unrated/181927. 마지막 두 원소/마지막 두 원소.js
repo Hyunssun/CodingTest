128033dd0edd3f9adc412a6ae20d12ac21bf0ce6
Hyunsun Park <sunny20829@gmail.com>
2023-08-16 09:15:41 +0900

    [unrated] Title: 마지막 두 원소, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,7 +1,4 @@
 function solution(num_list) {
-    let arr = num_list;
-    let last = num_list[num_list.length - 1];
-    let last_1 = num_list[num_list.length - 2];
-    last > last_1 ? arr.push(last - last_1) : arr.push(last * 2)
-    return arr;
+    const [a, b] = [...num_list].reverse()
+    return [...num_list, a > b ? a - b : a * 2];
 }
