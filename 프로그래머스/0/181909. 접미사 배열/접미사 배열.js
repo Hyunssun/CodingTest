0413f6609ac3b9067c3b419423f084095d0a0336
Hyunsun Park <sunny20829@gmail.com>
2024-02-02 08:22:33 +0900

    [level 0] Title: 접미사 배열, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
@@ -1,6 +1,5 @@
 function solution(my_string) {
     let answer = [];
-    let arr = my_string.split('')
     for(let i=my_string.length-1; i>=0; i--){
         answer.push(my_string.slice(i))
     }
