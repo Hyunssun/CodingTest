32212a77636f3473cc216383f2c27c07e6391e0f
Hyunsun Park <sunny20829@gmail.com>
2024-02-02 08:36:12 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -1,13 +1,7 @@
 function solution(my_string, is_suffix) {
-    let answer = 0;
     let arr = [];
     for(let i=my_string.length-1; i>=0; i--){
         arr.push(my_string.slice(i));
     }
-    arr.map((item)=>{
-        if(item===is_suffix){
-            return answer = 1;
-        }
-    })
-    return answer;
+    return arr.includes(is_suffix)?1:0;
 }
