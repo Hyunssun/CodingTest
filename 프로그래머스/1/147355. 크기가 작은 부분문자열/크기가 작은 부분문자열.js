7e828da43d2d3c4654a1294a9dab5f5449e5ba4f
Hyunsun Park <sunny20829@gmail.com>
2024-02-02 23:40:03 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -0,0 +1,11 @@
+function solution(t, p) {
+    var answer = 0;
+    for(let i=0; i<t.length; i++){
+        if(t.slice(i,i+p.length).length===p.length){       
+            if(t.slice(i,i+p.length)<=p){
+                answer++;
+            }
+        }
+    }
+    return answer;
+}
