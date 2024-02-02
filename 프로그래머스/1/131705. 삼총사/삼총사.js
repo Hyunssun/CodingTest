8f90fbb7dabdf8adf2d5a108fc882e64d21bc624
Hyunsun Park <sunny20829@gmail.com>
2024-02-03 00:03:15 +0900

    [level 1] Title: 삼총사, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.js
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -1,15 +1,14 @@
 function solution(number) {
     var answer = 0;
     for(let i=0; i<number.length; i++){
-        for(let j=0; j<number.length; j++){
-            for(let k=0; k<number.length; k++){
-                if(i !== j && j !== k && i !== k){
-                    if((number[i] + number[j] + number[k])===0){
-                        answer += ((number[i] + number[j] + number[k])===0)
-                    }
+        for(let j=i+1; j<number.length; j++){
+            for(let k=j+1; k<number.length; k++){
+                if(number[i]+number[j]+number[k]!==0){
+                    continue;
                 }
+                answer++;
             }
         }
     }
-    return answer/6;
+    return answer;
 }
