921259e5b12637a0da1f4d0b7f1478d3a0ecd4cc
Hyunsun Park <sunny20829@gmail.com>
2024-02-02 13:40:10 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,20 +1,7 @@
 function solution(s) {
     let answer = [];
-    let arr=s.split(' ')
-    let arr2=[]
-    
-    arr.map((item)=>{
-        arr2.push(item.split(''))
-    })
-    
-    arr2.map((item,index)=>{
-    let arr3=[]
-        item.map((it,idx)=>{
-            idx===0?
-                arr3.push(it.toUpperCase()):
-            arr3.push(it.toLowerCase());
-        })
-        answer.push(arr3.join(''))
+    s.split(' ').map((item)=>{
+        answer.push(item.charAt(0).toUpperCase()+item.substring(1).toLowerCase())
     })
     return answer.join(' ');
 }
