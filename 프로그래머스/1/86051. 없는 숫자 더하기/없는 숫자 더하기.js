e0af54b4de8936f1ecc734d66d0effa563db85d5
Hyunsun Park <sunny20829@gmail.com>
2024-02-02 17:10:21 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,11 +1,7 @@
 function solution(numbers) {
     var answer = 0;
-    let arr=[0,1,2,3,4,5,6,7,8,9]
-    numbers.map((item)=>{
-        arr=arr.filter((v)=>item!==v)
-    })
-    arr.map((item)=>{
-        answer+=item
-    })
+    for(let i = 0; i <= 9; i++) {
+        if(!numbers.includes(i)) answer += i;
+    }
     return answer;
 }
