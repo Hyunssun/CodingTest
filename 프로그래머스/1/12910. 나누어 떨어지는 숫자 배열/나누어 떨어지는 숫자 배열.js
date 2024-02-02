199f19baffb70231a30cb8aa557c0ad9b0acee11
Hyunsun Park <sunny20829@gmail.com>
2024-02-02 17:00:10 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.js
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.js
@@ -1,10 +1,4 @@
 function solution(arr, divisor) {
-    var answer = [];
-    arr.map((item)=>{
-        item % divisor===0 && answer.push(item)
-    })
-    if(answer.length===0){
-        answer.push(-1)
-    }
-    return answer.sort((a,b)=>a-b);
+    var answer = arr.filter((v)=>v%divisor===0)
+    return answer.length===0?[-1]:answer.sort((a,b)=>a-b);
 }
