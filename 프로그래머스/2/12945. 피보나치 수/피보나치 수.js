af253c46c9e4f08559e4fc6f155ad7817c5bcc83
Hyunsun Park <sunny20829@gmail.com>
2024-02-26 16:21:07 +0900

    [level 2] Title: 피보나치 수, Time: 6.88 ms, Memory: 38.7 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -1,7 +1,7 @@
 function solution(n) {
     var answer = 0;
     let arr=[]
-    for(let i=0; i<=n%1234567; i++){
+    for(let i=0; i<=n; i++){
         if(arr.length>1){
             arr.push(arr[i-1]%1234567+arr[i-2]%1234567)
         }else{
